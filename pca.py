dbc8cfe3135a215f1e62666ae553189d394b9f29
Changhwan Lee <chlee1252@gmail.com>
2020-12-12 14:50:52 +0900

    red detection -> faceDetection

--- pca.py
+++ pca.py
@@ -7,13 +7,18 @@ import time
 # import RPi.GPIO as GPIO #pin info
 # import time
 
+xml = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(xml)
+print(faceCascade)
+
+
 class Motor:
     def __init__(self, channel, offset):
         self.mChannel = channel
         self.offset = offset
         
         self.mPwm = PCA9685(address=0x40)
-        self.mPwm.set_pwm_freq(60)
+        self.mPwm.set_pwm_freq(50)
     
     def setPos(self, pos):
         pulse = (650-150) * pos / 180 + 150 + self.offset
@@ -24,8 +29,7 @@ class Motor:
         time.sleep(1)
 
 servo = Motor(channel=0, offset=-10)
-#servo = Motor(channel=1, offset=-10)
-servo.setPos(5)
+servo.setPos(90)
 
 
 with picamera.PiCamera() as camera:    # load picamera -> camera
@@ -48,23 +52,36 @@ with picamera.PiCamera() as camera:    # load picamera -> camera
 
             hsv_frame = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
             
-            #red color
-            low_red = np.array([161, 155, 84])
-            high_red = np.array([179, 255,255])
-            red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-            contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+            # face detection
+            gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
             
-            if not contours:
-                position = 90
-                x_medium = x_center
-            else:
-                for cnt in contours:
-                    (x, y, w, h) = cv2.boundingRect(cnt)
-                    #cv2.rectangle(stream.array, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(rows//3, cols//3),
+                flags = cv2.CASCADE_SCALE_IMAGE)
+            
+            if len(faces):
+                faces = np.sort(faces, axis=0)[::-1]
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(stream.array, (x,y), (x+w, y+h), (0, 255, 0), 2)
                     x_medium = (x+x+w) // 2
                     y_medium = (y+y+h) // 2
                     break
+                
+                    #red color
+#             low_red = np.array([161, 155, 84])
+#             high_red = np.array([179, 255,255])
+#             red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+#             contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#             contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+#             for cnt in contours:
+#                 (x, y, w, h) = cv2.boundingRect(cnt)
+#                 #cv2.rectangle(stream.array, (x,y), (x+w, y+h), (0, 255, 0), 2)
+#                 x_medium = (x+x+w) // 2
+#                 y_medium = (y+y+h) // 2
+#                 break
         
             cv2.line(stream.array, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
             cv2.line(stream.array, (0, y_medium), (800, y_medium), (0, 255, 0), 2)
@@ -74,18 +91,18 @@ with picamera.PiCamera() as camera:    # load picamera -> camera
             key = cv2.waitKey(1)
             if key == 27:
                 break
-            
+#             
             if 0 < position < 180:
-                if x_medium < x_center-20:
-                    position += 2
-                elif x_medium > x_center+20:
-                    position -= 2
+                if x_medium < x_center-30:
+                    position += 1
+                elif x_medium > x_center+30:
+                    position -= 1
             else:
                 x_medium = x_center
                 position = 90
-                
+# #                 
             print(position)
-                            
+#                             
             servo.setPos(position)
             # picamera required
             stream.seek(0)
@@ -95,5 +112,4 @@ with picamera.PiCamera() as camera:    # load picamera -> camera
 #cap.release()
 servo.cleanUp()
 cv2.destroyAllWindows()
-
 
